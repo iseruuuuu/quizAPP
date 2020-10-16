52818a5662628d902dfc11e48bf7d93821bde1b1
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2020-10-16 16:43:17 +0900

    Initial Commit

---
+++ quizAPP/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  quizAPP
+//
+//  Created by user on 2020/10/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
